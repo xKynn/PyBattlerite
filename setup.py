2e425967efd6ac598542e2ef720009060caa5052
Ary Y <gtownx36@gmail.com>
2017-11-30 23:44:03 +0530

    Version changed

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = "asyncbattlerite",
     packages = ['AsyncBattlerite'],
-    version = "0.2",
+    version = "0.4",
     description= "A non-blocking async wrapper for the madglory Battlerite API",
     author = "xKynn",
     author_email = "xkynn@github.com",
     url = "https://github.com/xKynn/AsyncBattlerite",
-    download_url = "https://github.com/xKynn/AsyncBattlerite/archive/0.2.tar.gz",
+    download_url = "https://github.com/xKynn/AsyncBattlerite/archive/0.4.tar.gz",
     keywords = ['battlerite', 'asyncbattlerite', 'async-battlerite', 'async_battlerite'],
     classifiers = []
 )
