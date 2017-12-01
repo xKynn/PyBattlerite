86371df765b3da649bdd27327a31bd4cb7eb064f
xKynn <gtownx36@gmail.com>
2017-12-01 23:40:13 +0530

    Small bugfix in a ternary op

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = "asyncbattlerite",
     packages = ['AsyncBattlerite'],
-    version = "0.5.1",
+    version = "0.5.2",
     description= "A non-blocking async wrapper for the madglory Battlerite API",
     author = "xKynn",
     author_email = "xkynn@github.com",
     url = "https://github.com/xKynn/AsyncBattlerite",
-    download_url = "https://github.com/xKynn/AsyncBattlerite/archive/0.5.1.tar.gz",
+    download_url = "https://github.com/xKynn/AsyncBattlerite/archive/0.5.2.tar.gz",
     keywords = ['battlerite', 'asyncbattlerite', 'async-battlerite', 'async_battlerite'],
     classifiers = []
 )
