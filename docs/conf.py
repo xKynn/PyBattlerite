a29782550602c71b120ca36956807e5efec2c8fe
xKynn <gtownx36@gmail.com>
2017-12-08 23:16:39 +0530

    Relative docs path

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
 
 numpydoc_show_class_members = False
 # -- General configuration ------------------------------------------------
